6ce974184e8b761bbd81e408bad0bf1a44c7cea1
Lucas Aguirre <aguirrelucas2017@outlook.es>
2022-04-28 16:11:09 -0300

    change jueves 28 - 16:11

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-undef */
-const path = require('path')
-const webpack = require('webpack')
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   entry: './src/main.js',
@@ -16,8 +15,8 @@ module.exports = {
         use: [
           'vue-style-loader',
           'css-loader'
-        ]
-      }, {
+        ],
+      },      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
@@ -42,7 +41,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      vue$: 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js'
     },
     extensions: ['*', '.js', '.vue', '.json']
   },
@@ -57,9 +56,9 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
-// http://vue-loader.vuejs.org/en/workflow/production.html
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
